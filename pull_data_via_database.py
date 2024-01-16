0c20d4561a7fef6d98e3600362bddaf0a16d09f9
Feyaaz <feyaazc94@gmail.com>
2024-01-16 02:49:21 +0000

    Update SQL Script For Reusability

--- pull_data_via_database.py
+++ pull_data_via_database.py
@@ -52,22 +52,49 @@ for res in response["fixtures"]["fixtures"]:
     for x in res["games"]:
         final_json = construct_json(res, x["results"])
 
-        cursor.execute(
-            """
-            INSERT INTO tennis_data (tournament_name, last_updated, event_name, start_time, outcome_name1, odds1, outcome_name2, odds2)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        """,
-            (
-                final_json["tournament_name"],
-                final_json["last_updated"],
-                final_json["events"][0]["event_name"],
-                final_json["events"][0]["start_time"],
-                final_json["events"][0]["outcomes"][0]["outcome_name"],
-                final_json["events"][0]["outcomes"][0]["odds"],
-                final_json["events"][0]["outcomes"][1]["outcome_name"],
-                final_json["events"][0]["outcomes"][1]["odds"],
-            ),
-        )
+        cursor.execute('''
+            SELECT * FROM tennis_data WHERE event_name=?
+        ''', (final_json["events"][0]["event_name"],))
+
+        existing_record = cursor.fetchone()
+
+        if existing_record:
+            # Update the existing record
+            cursor.execute(
+                """
+                UPDATE tennis_data
+                SET tournament_name=?, last_updated=?, start_time=?, outcome_name1=?, odds1=?, outcome_name2=?, odds2=?
+                WHERE event_name=?
+            """,
+                (
+                    final_json["tournament_name"],
+                    final_json["last_updated"],
+                    final_json["events"][0]["start_time"],
+                    final_json["events"][0]["outcomes"][0]["outcome_name"],
+                    final_json["events"][0]["outcomes"][0]["odds"],
+                    final_json["events"][0]["outcomes"][1]["outcome_name"],
+                    final_json["events"][0]["outcomes"][1]["odds"],
+                    final_json["events"][0]["event_name"],
+                ),
+            )
+        else:
+            # Insert a new record
+            cursor.execute(
+                """
+                INSERT INTO tennis_data (tournament_name, last_updated, event_name, start_time, outcome_name1, odds1, outcome_name2, odds2)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+                (
+                    final_json["tournament_name"],
+                    final_json["last_updated"],
+                    final_json["events"][0]["event_name"],
+                    final_json["events"][0]["start_time"],
+                    final_json["events"][0]["outcomes"][0]["outcome_name"],
+                    final_json["events"][0]["outcomes"][0]["odds"],
+                    final_json["events"][0]["outcomes"][1]["outcome_name"],
+                    final_json["events"][0]["outcomes"][1]["odds"],
+                ),
+            )
 
         conn.commit()
 
